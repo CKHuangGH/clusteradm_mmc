dd934a381cb23ab354c735b07d5488cad5f419d7
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-07-31 21:58:50 +0200

    Update exec.go

--- pkg/cmd/join/exec.go
+++ pkg/cmd/join/exec.go
@@ -143,7 +143,7 @@ func (o *Options) complete(cmd *cobra.Command, args []string) (err error) {
 	agentNamespace := AgentNamespacePrefix + "agent"
 	// McKlusterletName := "klusterlet-" + o.clusterName + "-" + rfc1035Domain
 	// McNamespace := o.clusterName + "-" + helpers.RandStringRunes_az09(6)
-	McKlusterletName := "klusterlet-" + rfc1035Domain
+	// McKlusterletName := "klusterlet-" + rfc1035Domain
 	// McNamespace := o.clusterName + "-" + rfc1035Domain
 	McNamespace := "mgmt-" + rfc1035Domain
 	o.values = Values{
