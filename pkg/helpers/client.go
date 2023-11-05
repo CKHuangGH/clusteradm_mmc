0ed964da796526cf0b238b52619c4e6c2ee68f2b
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-11-05 21:38:05 +0100

    Update client.go

--- pkg/helpers/client.go
+++ pkg/helpers/client.go
@@ -213,7 +213,7 @@ func GetBootstrapTokenFromSA(ctx context.Context, kubeClient kubernetes.Interfac
 		CreateToken(ctx, config.BootstrapSAName, &authv1.TokenRequest{
 			Spec: authv1.TokenRequestSpec{
 				// token expired in 1 hour
-				ExpirationSeconds: pointer.Int64(10800),
+				ExpirationSeconds: pointer.Int64(43200),
 			},
 		}, metav1.CreateOptions{})
 	if err != nil {
